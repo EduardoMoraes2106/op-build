de0edbab041d4581a9388b18bd1b9fdd87322248
erickgamer56 <ericknovaes56@gmail.com>
2023-06-11 16:47:20 -0300

    'base-login'

--- App/Controller/Pages/Login.php
+++ App/Controller/Pages/Login.php
@@ -4,9 +4,16 @@ namespace App\Controller\Pages;
 use \App\Utils\View;
 
 class Login extends Main{
+
+    private static function getForm(){
+        $content = View::render('Pages/login/components/form');
+        return $content;
+    }
+
     public static function getLogin(){
         $content= View::render('Pages/login',[
             'name' => 'OP-Build',
+            'form' => self::getForm()
         ]);
         return parent::getMain('Login - OPBuild', $content);
     }
